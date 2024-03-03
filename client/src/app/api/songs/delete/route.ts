3b75324e07827307b19ca7fb671f22a7d256ec1e
Alex <trevan98@gmail.com>
2024-03-03 22:37:59 +0300

    migrations for ci

---
+++ client/src/app/api/songs/delete/route.ts
@@ -0,0 +1,6 @@
+//click delete song from my music
+//fetch db and check if user exist
+//then delete song from user music in db
+//check if music has user relations
+//if there is none, then send request to music-service and delete folder with a music
+
