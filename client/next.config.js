5edc662a0c9d0aa508aa8ac9e73e880af45d056d
Alex <trevan98@gmail.com>
2024-03-19 05:25:53 +0300

    cors

--- client/next.config.js
+++ client/next.config.js
@@ -18,6 +18,23 @@ const nextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "https://tuneaudio.fun" },
+          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
 };
 module.exports = nextConfig;
 // module.exports = withBundleAnalyzer(nextConfig)
