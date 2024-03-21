a81c68ddabc88f7e0c8f90dbe72c83bc4f772c0a
Alex <trevan98@gmail.com>
2024-03-21 09:15:34 +0300

    testing api

--- music-service/src/main.py
+++ music-service/src/main.py
@@ -22,6 +22,10 @@ app.add_middleware(
 app.mount("/audio", StaticFiles(directory="audio"), name="audio")
 app.include_router(search.router)
 
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
