91ea26a7bd847de40de9e1e681baf1a847554baf
alex <trevan98@gmail.com>
2024-07-02 12:47:58 +0300

    fix play or pause styles and positioning

--- client/src/app/(music)/_/components/icons/player/Pause.tsx
+++ client/src/app/(music)/_/components/icons/player/Pause.tsx
@@ -4,8 +4,8 @@ export function Pause(props: SVGProps<SVGSVGElement>) {
   return (
     <svg
       {...props}
-      width="26"
-      height="26"
+      width="23"
+      height="23"
       viewBox="0 0 26 26"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
